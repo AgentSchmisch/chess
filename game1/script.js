e7d476250598d0d6fb7a90cb551b7c6abeb97f65
Florian Schmid <florian.s@ventureal.com>
2023-11-17 15:09:27 +0100

    🚧 finished audio player

--- game1/script.js
+++ game1/script.js
@@ -1,5 +1,7 @@
+//TODO: add possibility to gain a sword to fight the guard of the cave
+
 const startbtn = document.querySelector("#start");
-startbtn.addEventListener("click", startgame);
+startbtn.addEventListener("click", startGame);
 
 const tavernbtn = document.querySelector("#tavern");
 tavernbtn.addEventListener("click", tavern);
@@ -7,8 +9,13 @@ tavernbtn.addEventListener("click", tavern);
 const cavebtn = document.querySelector("#cave");
 cavebtn.addEventListener("click", cave);
 
-const intro = document.querySelector("#dialog");
+const hotelbtn = document.querySelector("#hotel");
+hotelbtn.addEventListener("click", hotel);
 
+const stablebtn = document.querySelector("#shed");
+stablebtn.addEventListener("click", shed);
+
+const intro = document.querySelector("#dialog");
 
 const inventory = document.querySelector("#inventory");
 
@@ -16,6 +23,8 @@ const dialogbox = document.querySelector(".dialogbox");
 
 const username = document.querySelector("#username");
 const weapon = document.querySelector("#weapon");
+
+let audioPlayer = new Audio();
 
 const player = {
     name:"",
@@ -25,17 +34,23 @@ const player = {
 
 
 function play(theme) {
-    let audio = new Audio();
+    let track = ""
+    console.log(theme)
     switch (theme){
         case "ambient":
+            console.log("playing ambient")
+            audioPlayer.pause();
             track = "./audio/ambient.mp3";
-            audio.src = track;
-            audio.play();
+            break;
+            
         case "tavern":
+            console.log("playing tavern")
+            audioPlayer.pause();
             track = "./audio/tavern.mp3";
-            audio.src = track;
-            audio.play();
+            break;
     }
+    audioPlayer.src = track;
+    audioPlayer.play();
 
 }
 
@@ -50,11 +65,16 @@ function remove_startcontainer(){
     startcontainer.style.display="none";
 }
 
-function cave(){
+function cave() {
     remove_startcontainer();
     changeBackground('./imgs/CaveEntry.avif')
     if (player.items.includes("sword")) {
         intro.innerHTML="You enter the cave. It is dark and damp. You encounter the guard of the treasure. You have a sword!";
+        setTimeout(() => {
+            //Fight scene
+            intro.innerHTML="You fight the guard and win! You take the treasure and head back to the tavern to celebrate. YOU WIN!"
+            changeBackground('./imgs/TavernPatrons.jpg');
+        }, 7000);
     }
     else {
         intro.innerHTML="You enter the cave. It is dark and damp. You encounter the guard of the treasure. You dont have a weapon. GAME OVER"
@@ -64,14 +84,26 @@ function cave(){
 
 function tavern(){
     remove_startcontainer();
+    play("tavern");
     document.querySelector("#options").style.display = "none";
-    intro.innerHTML="You enter the tavern. It is a small, cozy place with a fire crackling. You see a bartender and a few patrons. You get a few drinks and get talking with the patrons. They tell you about a cave nearby that is rumored to be filled with treasure. You decide to go check it out. You leave the tavern and head towards the cave. You see a sign that says 'Beware of the Cave'. Do you enter the cave or go back to the tavern?";
+    intro.innerHTML = "You enter the tavern. It is a small, cozy place with a fire crackling. You see a bartender and a few patrons. You get a few drinks and get talking with the patrons. They tell you about a cave nearby that is rumored to be filled with treasure. You decide to go check it out. You leave the tavern and head towards the cave. You see a sign that says 'Beware of the Cave'. Do you enter the cave or go back to the tavern?";
     changeBackground('./imgs/TavernPatrons.jpg');
-    //TODO: add fight scene
+    
+
+    //TODO: add function cave else tavern
+}
+
+function hotel(){
+
+}
+
+function shed(){
+    intro.innerHTML = "While taking your way to the shed, you encounter a sign that says 'Beware of the Cave'. Your curiousity gets sparked by the sign and you turn to the tavern to gather more information."
+    tavern();
 }
 
 
-function startgame(){
+function startGame(){
     let playerName = document.querySelector("#playerName").value;
 
     if(playerName !== "" && typeof playerName === "string"){
@@ -88,7 +120,7 @@ function startgame(){
     }
 
     else {
-        intro.innerHTML="Please enter a name!";
+        intro.innerHTML = "Please enter a name!";
         return;
     }
 }
