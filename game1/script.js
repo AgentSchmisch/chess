56038d0d693de57fe2f05a96ce1537ca7dc2dcf1
Florian Schmid <florian.s@ventureal.com>
2023-11-19 20:26:47 +0100

    :sparkles: added town sqlare interaction

--- game1/script.js
+++ game1/script.js
@@ -15,6 +15,11 @@ hotelbtn.addEventListener("click", hotel);
 const stablebtn = document.querySelector("#shed");
 stablebtn.addEventListener("click", shed);
 
+document.querySelector("#talk").addEventListener("click", talkToPerson);
+
+document.querySelector("#lookAround").addEventListener("click", townOverview);
+
+
 const intro = document.querySelector("#dialog");
 
 const inventory = document.querySelector("#inventory");
@@ -23,6 +28,8 @@ const dialogbox = document.querySelector(".dialogbox");
 
 const username = document.querySelector("#username");
 const weapon = document.querySelector("#weapon");
+
+const town = document.querySelector("#town");
 
 let audioPlayer = new Audio();
 
@@ -64,6 +71,30 @@ function remove_startcontainer(){
     const startcontainer = document.querySelector(".startcontainer");
     startcontainer.style.display="none";
 }
+function displayMenuButtons(){
+    tavernbtn.style.display="block";
+    cavebtn.style.display="block";
+    hotelbtn.style.display="block";
+    stablebtn.style.display="block";
+
+    tavernbtn.innerHTML="Tavern";
+    cavebtn.innerHTML="Cave";
+    hotelbtn.innerHTML="Hotel";
+    stablebtn.innerHTML="Stable";
+
+    tavernbtn.addEventListener("click", tavern);
+    cavebtn.addEventListener("click", cave);
+    hotelbtn.addEventListener("click", hotel);
+    stablebtn.addEventListener("click", shed);
+
+}
+
+function hideMenuButtons(){
+    tavernbtn.style.display="none";
+    cavebtn.style.display="none";
+    hotelbtn.style.display="none";
+    stablebtn.style.display="none";
+}
 
 function cave() {
     remove_startcontainer();
@@ -74,32 +105,32 @@ function cave() {
             //Fight scene
             intro.innerHTML="You fight the guard and win! You take the treasure and head back to the tavern to celebrate. YOU WIN!"
             changeBackground('./imgs/TavernPatrons.jpg');
-        }, 7000);
+        }, 10000);
     }
     else {
         intro.innerHTML="You enter the cave. It is dark and damp. You encounter the guard of the treasure. You dont have a weapon. GAME OVER"
     }
-
 }
 
 function tavern(){
     remove_startcontainer();
     play("tavern");
     document.querySelector("#options").style.display = "none";
-    intro.innerHTML = "You enter the tavern. It is a small, cozy place with a fire crackling. You see a bartender and a few patrons. You get a few drinks and get talking with the patrons. They tell you about a cave nearby that is rumored to be filled with treasure. You decide to go check it out. You leave the tavern and head towards the cave. You see a sign that says 'Beware of the Cave'. Do you enter the cave or go back to the tavern?";
+    intro.innerHTML = "You enter the tavern. It is a small, cozy place with a fire crackling. You see a bartender and a few patrons. You get a few drinks and get talking with the patrons. They tell you about a cave nearby that is rumored to be filled with treasure.";
     changeBackground('./imgs/TavernPatrons.jpg');
-    
-
-    //TODO: add function cave else tavern
+    setTimeout(() => {
+        intro.innerHTML = "You decide to go check it out. You leave the tavern and head towards the cave. Before leaving, the patrons give you a sword. On your way to the cave you see a sign that says 'Beware of the Cave'. Do you enter the cave or go back to the tavern?"
+        setTimeout(cave, 10000)
+    }, 10000);
 }
 
 function hotel(){
-
+    intro.innerHTML = "hotel scene"
 }
 
 function shed(){
     intro.innerHTML = "While taking your way to the shed, you encounter a sign that says 'Beware of the Cave'. Your curiousity gets sparked by the sign and you turn to the tavern to gather more information."
-    tavern();
+    setTimeout(tavern, 5000);
 }
 
 
@@ -107,22 +138,40 @@ function startGame(){
     let playerName = document.querySelector("#playerName").value;
 
     if(playerName !== "" && typeof playerName === "string"){
-        intro.innerHTML=`Welcome ${playerName}! You are a young adventurer who has just arrived in the town of Hogsmeade. You are looking for a place to stay for the night. You see a tavern, a hotel, and a stable. Which do you choose?`;
-        const options = document.querySelector("#options").style.display = "flex"; 
+        intro.innerHTML=`Welcome ${playerName}! You are a young adventurer who has just arrived in the town of Hogsmeade. You are looking for a place to stay for the night. You see a Person in front of a House, do you want to talk to him or take a look around the town?`;
+        hideMenuButtons();
+        document.querySelector("#options").style.display = "flex";
+        town.style.display="flex";
 
         play("ambient");
-        
-        inventory.style.display="flex";
-        dialogbox.style.justifyContent="space-between";
+
         player.name = playerName;
         username.innerHTML = `Name: ${player.name}`;
+
         remove_startcontainer();
     }
 
     else {
         intro.innerHTML = "Please enter a name!";
         return;
-    }
+    }}
+
+function talkToPerson(){
+    intro.innerHTML = "You talk to the person and he tells you about a tavern, a hotel and a shed where do you want to go?";
+    displayMenuButtons();
+    town.style.display="none";
+    inventory.style.display="flex";
+    dialogbox.style.justifyContent="space-between";
+
+}
+
+function townOverview(){
+    intro.innerHTML = "You look around the town and see a tavern, a hotel, a shed and a cave. Where do you want to go?";
+    displayMenuButtons();
+    changeBackground('./imgs/background.jpg');
+    inventory.style.display="flex";
+    dialogbox.style.justifyContent="space-between";
+
 }
 
 
