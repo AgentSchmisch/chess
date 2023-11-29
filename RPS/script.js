109c7389d8c0017f48117fa6a4000c84fb8cd392
Florian Schmid <florian.schmid2002@outlook.com>
2023-11-29 14:15:23 +0100

    🚧 creating game logic

--- RPS/script.js
+++ RPS/script.js
@@ -2,10 +2,21 @@ document.querySelector("#rock").addEventListener("click", rock)
 document.querySelector("#paper").addEventListener("click", paper)
 document.querySelector("#scissors").addEventListener("click", scissors)
 
+const player = {
+    wins : 0,
+    attempts : 0,
+    action:"",
+}
+
+
+const computer = Object({}, player)
+
 
 function rock(){
-
-    console.log("rock")
+    player.action = "rock";
+    computer.action = playEnemy();
+    player.attempts++;
+    evaluateWinner(player.action,computer.action)
     return ""
 }
 
@@ -26,24 +37,61 @@ function playEnemy(){
 
     let actions = ["rock", "paper", "scissors"]
 
-
     console.log(value)
     console.log(actions[value])
 
     return actions[value]
 }
 
-function evaluateWinner(player, computer){
+function evaluateWinner(_player, _computer){
     //Paper -> Rock
     //Rock -> Scissors
     //Rock -> Scissors
 
     //evaluate the action of the player and based on that decide if the computer has won or the player has won
+    if (_player === "rock"){
+        if(_computer === "rock"){
+            console.log("it's a tie")
+        }
 
+        else if(_computer === "paper"){
+            console.log("computer wins")
+        }
+
+        else if(_computer === "scissors"){
+            player.wins++;
+        }
+    }
+    else if(_player === "paper"){
+        if(_computer === "rock"){
+            player.wins++;
+        }
+
+        else if(_computer === "paper"){
+            console.log("its a tie")
+        }
+
+        else if(_computer === "scissors"){
+            console.log("computer wins")
+        }
+    }
+    else if(_player === "scissors"){
+        if(_computer === "rock"){
+            console.log("computer wins")
+        }
+
+        else if(_computer === "paper"){
+            player.wins++;
+        }
+
+        else if(_computer === "scissors"){
+            console.log("its a tie")
+        }
+    }
 
     let actions = ["rock", "paper", "scissors"]
 
-    let winner
+    let winner = null
 
     return winner
 }
