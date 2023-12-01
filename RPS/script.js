3a935cf4e2e56e73f7543a765e45d42cef0a319d
Florian Schmid <florian.schmid2002@outlook.com>
2023-12-01 17:13:05 +0100

    :sparkles: added animation for computer

--- RPS/script.js
+++ RPS/script.js
@@ -2,124 +2,196 @@ document.querySelector("#rock").addEventListener("click", rock)
 document.querySelector("#paper").addEventListener("click", paper)
 document.querySelector("#scissors").addEventListener("click", scissors)
 
-const player = {
-    wins : 0,
-    attempts : 0,
-    action:"",
+function play(theme) {
+    let audioPlayer = new Audio();
 
+    switch (theme) {
+        case "loose":
+            audioPlayer.pause();
+            track = "./audio/loose.mp3";
+            break;
+        case "rock":
+            audioPlayer.pause();
+            track = "./audio/rock.mp3";
+            break;
+        case "paper":
+            audioPlayer.pause();
+            track = "./audio/paper.mp3";
+            break;
+        case "scissors":
+            audioPlayer.pause();
+            track = "./audio/scissors.mp3";
+            break;
+        case "win":
+            audioPlayer.pause();
+            track = "./audio/win.mp3"
+    }
+}
+
+
+
+const player = {
+    wins: 0,
+    attempts: 0,
+    action: "",
 
 }
 
 const computer = Object.assign({}, player)
 
-player.update = function(){
-    document.querySelector("#scorePlayer").innerHTML = `Player: ${this.wins} ${this.wins < 2 ? "Win" : "Wins" }`;
+player.update = function () {
+    document.querySelector("#scorePlayer").innerHTML = `Player: ${this.wins} ${this.wins < 2 ? "Win" : "Wins"}`;
 }
 
-computer.update = function(){
-    document.querySelector("#scoreComputer").innerHTML = `Computer: ${this.wins} ${this.wins < 2 ? "Win" : "Wins" }`;
+computer.update = function () {
+    document.querySelector("#scoreComputer").innerHTML = `Computer: ${this.wins} ${this.wins < 2 ? "Win" : "Wins"}`;
 }
 
-player.win = function(){
+player.win = function () {
     this.wins++;
+    this.update();
+    play("win")
+    this.blink();
+
+    eval = document.querySelector("#evaluation")
+    eval.innerHTML = "Player wins"
+    eval.style.backgroundColor = "lightgreen"
     //TODO: add audio to player if he wins
 }
-player.loose = function(){
+player.loose = function () {
+    computer.wins++;
+    computer.update()
+
+    play("loose")
+    eval = document.querySelector("#evaluation")
+    eval.innerHTML = "Computer wins"
+    eval.style.backgroundColor = "salmon"
 
     //TODO: add audio if the player looses
 }
 
+player.tie = function(){
+    eval = document.querySelector("#evaluation")
+    eval.innerHTML = "It's a tie"
+    eval.style.backgroundColor = "white"
+}
 
-function rock(){
+player.blink = function(){
+    document.querySelector("#scorePlayer").classList.add("flashName");
+    setTimeout(()=>{
+        document.querySelector("#scorePlayer").classList.remove("flashName");
+    }, 3100);
+}
+
+function rock() {
     player.action = "rock";
+    play("rock");
     computer.action = playEnemy();
     player.attempts++;
-    evaluateWinner(player.action, computer.action)
-    console.log("Computer: " + computer.action + "/ Player: " + player.action)
+    setTimeout(()=>{
+        evaluateWinner(player.action, computer.action)
+        console.log("Computer: " + computer.action + "/ Player: " + player.action)
+    }, 2000);
     return ""
 }
 
-function paper(){
+function paper() {
     player.action = "paper";
+    play("paper")
     computer.action = playEnemy();
     player.attempts++;
-    evaluateWinner(player.action, computer.action)
-    console.log("Computer: " + computer.action + "/ Player: " + player.action)
+    setTimeout(()=>{
+        evaluateWinner(player.action, computer.action)
+        console.log("Computer: " + computer.action + "/ Player: " + player.action)
+    }, 2000);
     return ""
 }
 
-function scissors(){
+function scissors() {
     player.action = "scissors";
+    play("scissors")
     computer.action = playEnemy();
     player.attempts++;
-    evaluateWinner(player.action, computer.action)
-    console.log("Computer: " + computer.action + "/ Player: " + player.action)
+    setTimeout(()=>{
+        evaluateWinner(player.action, computer.action)
+        console.log("Computer: " + computer.action + "/ Player: " + player.action)
+    }, 2000);
+
     return ""
 }
 
-function playEnemy(){
+function playEnemy() {
     let value = Math.floor(Math.random() * (3 - 0) + 0)
 
-    let actions = [["rock","fa-solid fa-hand-back-fist fa-5x"], ["paper","fa-solid fa-hand fa-5x"], ["scissors","fa-solid fa-hand-scissors fa-5x"]]
-    document.querySelector("#computer").className = actions[value][1];
+    let actions = [["rock", "fa-solid fa-hand-back-fist fa-5x"], ["paper", "fa-solid fa-hand fa-5x"], ["scissors", "fa-solid fa-hand-scissors fa-5x"]]
+    let i = 1;
+    let count = 0;
+    //Animation of the computer
+    let intervalID = setInterval(() => {
+        console.log(i)
+        document.querySelector("#computer").className = actions[i-1][1];
+        if (i===actions.length && count != 2){
+            count++;
+            i=1;
+        }
+        else if(count===2){
+            clearInterval(intervalID)
+            document.querySelector("#computer").className = actions[value][1];  
+
+        }
+        i++;
+        
+    }, 300);
+
     return actions[value][0]
+
 }
 
-function evaluateWinner(_player, _computer){
+function evaluateWinner(_player, _computer) {
     //Paper -> Rock
     //Rock -> Scissors
     //Rock -> Scissors
 
     //evaluate the action of the player and based on that decide if the computer has won or the player has won
-    if (_player === "rock"){
-        if(_computer === "rock"){
-            console.log("it's a tie")
+    if (_player === "rock") {
+        if (_computer === "rock") {
+            player.tie();
         }
 
-        else if(_computer === "paper"){
-            console.log("computer wins")
-            computer.wins++;
-            computer.update();
+        else if (_computer === "paper") {
+            player.loose();
         }
 
-        else if(_computer === "scissors"){
-            player.wins++;
-            player.update();
+        else if (_computer === "scissors") {
+            player.win();
         }
     }
 
-    else if(_player === "paper"){
-        if(_computer === "rock"){
-            player.wins++;
-            player.update();
+    else if (_player === "paper") {
+        if (_computer === "rock") {
+            player.win();
         }
 
-        else if(_computer === "paper"){
-            console.log("its a tie")
+        else if (_computer === "paper") {
+            player.tie();
         }
 
-        else if(_computer === "scissors"){
-            console.log("computer wins")
-            computer.wins++;
-            computer.update();
+        else if (_computer === "scissors") {
+            player.loose();
         }
     }
 
-    else if(_player === "scissors"){
-        if(_computer === "rock"){
-            console.log("computer wins")
-            computer.wins++;
-            computer.update();
+    else if (_player === "scissors") {
+        if (_computer === "rock") {
+            player.loose();
         }
 
-        else if(_computer === "paper"){
-            player.wins++;
-            player.update();
+        else if (_computer === "paper") {
+            player.win();
         }
 
-        else if(_computer === "scissors"){
-            console.log("its a tie");
+        else if (_computer === "scissors") {
+            player.tie();
         }
     }
-
 }
