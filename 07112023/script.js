60b1ba620906ef7d80511a43dbf35bdb579d8a59
Florian Schmid <78489341+AgentSchmisch@users.noreply.github.com>
2024-01-14 22:55:57 +0100

    Delete 07112023 directory

--- 07112023/script.js
+++
@@ -1,9 +0,0 @@
-console.log("hello world")
-
-function clicked(){
-    console.log("THANKS FOR CLICKING MEEE")
-}
-
-const browserConsole = document.querySelector('#browserConsole');
-console.log(browserConsole)
-browserConsole.innerHTML = browserConsole;
